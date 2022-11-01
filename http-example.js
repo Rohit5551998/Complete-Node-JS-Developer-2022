0163059c0f9aab69777dfae3f8a541449d5c68f2
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-01 15:04:29 +0530

    http-example

---
+++ http-example.js
@@ -0,0 +1,26 @@
+// const http = require('https');
+// ECMA equivalent
+// const { request } = require('https');
+
+// // const req = http.request('https://www.google.com', (res) => {
+// const req = request('https://www.google.com', (res) => {
+//     res.on('data', (chunk) => {
+//         console.log(`Data chunk: ${chunk}`);
+//     });
+//     res.on('end', () => {
+//         console.log('No more data');
+//     });
+// });
+
+// req.end();
+
+const { get } = require('https');
+
+get('https://www.google.com', (res) => {
+    res.on('data', (chunk) => {
+        console.log(`Data chunk: ${chunk}`);
+    });
+    res.on('end', () => {
+        console.log('No more data');
+    });
+});
