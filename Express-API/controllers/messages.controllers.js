5f4678fb775d9401f8856f239219f08ea6d0c6cd
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-02 15:11:43 +0530

    Express Layouts

--- Express-API/controllers/messages.controllers.js
+++ Express-API/controllers/messages.controllers.js
@@ -1,8 +1,12 @@
 const path = require('path');
 
 function getMessages(req, res) {
-    res.sendFile(path.join(__dirname, '..', 'public', 'images','skimountain.jpg'));
     // res.json('<ul><li>Hello Albert!</li></ul>');
+    // res.sendFile(path.join(__dirname, '..', 'public', 'images','skimountain.jpg'));
+    res.render('messages', {
+        title: 'Messages to my Friend!',
+        friend: 'Elon Musk',
+    });
 }
 
 function postMessage(req, res) {
