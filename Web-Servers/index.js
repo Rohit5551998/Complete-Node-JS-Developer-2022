43d9f4cde8aaea7c7e27d322cd38ce3127b16fa2
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-02 10:55:11 +0530

    Parameterized URLs

--- Web-Servers/index.js
+++ Web-Servers/index.js
@@ -4,18 +4,36 @@ const PORT = 3000;
 
 const server = http.createServer();
 
-server.on('request', (req, res) => {
-    if (req.url === '/friends') {
-    // res.writeHead(200, {
-    //     'Content-Type': 'applicatio/json',
-    // });
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({
+const friends = [
+    {
+        id: 0,
+        name: 'Nikola Tesla',
+    },
+    {
         id: 1,
         name: 'Sir Issac Newton',
-    }));
-    } else if (req.url === '/messages') {
+    },
+    {
+        id: 2,
+        name: 'Albert Einstein',
+    }
+]
+
+server.on('request', (req, res) => {
+    const items = req.url.split('/');
+    // /friends/2 => ['', 'friends', '2']
+    if (items[1] === 'friends') {
+        // res.writeHead(200, {
+        //     'Content-Type': 'applicatio/json',
+        // });
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        if (items.length === 3) {
+            res.end(JSON.stringify(friends[Number(items[2])]));
+        } else {
+            res.end(JSON.stringify(friends));
+        }
+    } else if (items[1] === 'messages') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<body>');
