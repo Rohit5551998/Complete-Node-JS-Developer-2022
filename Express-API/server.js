6d8d408e186dd9b965fda9a7a5ace435e97e5732
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-02 12:34:22 +0530

    POST request

--- Express-API/server.js
+++ Express-API/server.js
@@ -23,6 +23,23 @@ app.use((req, res, next) => {
     console.log(`${req.method} ${req.url} ${delta}ms`);
 });
 
+app.use(express.json());
+
+app.post('/friends', (req, res) => {
+    if (!req.body.name) {
+        return res.status(400).json({
+            error: "Missing Friend Name",
+        });
+    }
+    const newFriend = {
+        id: friends.length,
+        name: req.body.name,
+    }
+    friends.push(newFriend);
+    
+    res.json(newFriend);
+});
+
 app.get('/friends', (req, res) => {
     // res.send('Heeeeellloooooo!!!!!');
     res.json(friends)
