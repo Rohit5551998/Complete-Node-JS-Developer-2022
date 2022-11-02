0e4ad47dedaf9095aaed90c335748114a4af52b9
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-02 12:02:55 +0530

    Route Parameters and Dev Depnedencies

--- Express-API/server.js
+++ Express-API/server.js
@@ -15,9 +15,21 @@ const friends = [
     }
 ];
 
-app.get('/', (req, res) => {
+app.get('/friends', (req, res) => {
     // res.send('Heeeeellloooooo!!!!!');
     res.json(friends)
+});
+
+app.get('/friends/:friendId', (req, res) => {
+    const friendId = Number(req.params.friendId);
+    const friend = friends[friendId]
+    if (friend) {
+        res.status(200).json(friend);
+    } else {
+        res.status(404).json({
+            error: 'Friend does not exist!!!'
+        });
+    }
 });
 
 app.get('/messages', (req, res) => {
