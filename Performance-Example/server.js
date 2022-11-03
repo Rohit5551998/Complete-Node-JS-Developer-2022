c1b65adcf7718c0510fdb461767e71855094139a
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-03 16:38:58 +0530

    Extending Number of Forks

--- Performance-Example/server.js
+++ Performance-Example/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cluster = require('cluster');
+const os = require('os');
 
 const app = express();
 
@@ -23,8 +24,10 @@ app.get('/timer', (req, res) => {
 
 if (cluster.isMaster) {
     console.log('Master has been started ...');
-    cluster.fork();
-    cluster.fork();
+    const NUM_WORKERS = os.cpus().length;
+    for (i=0; i<NUM_WORKERS; i++){
+        cluster.fork();
+    }
 } else {
     console.log('Worker process started ...');
     app.listen(3000);
