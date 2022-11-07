fc51ca080269a610d98bebee5f7100cfd59f6598
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-07 17:20:21 +0530

    Paginating Launches Endpoint

--- NASA Project/server/src/routes/launches/launches.controllers.js
+++ NASA Project/server/src/routes/launches/launches.controllers.js
@@ -1,7 +1,12 @@
 const { existsLaunchWithId, getAllLaunches, scheduleNewLaunch, abortLaunchById } = require('../../models/launches.models');
+const { getPagination } = require('../../services/query');
+
 
 async function httpGetAllLaunches(req, res) {
-    return res.status(200).json(await getAllLaunches());
+    // console.log(req.query);
+    const { skip, limit } = getPagination(req.query);
+    const launches = await getAllLaunches(skip, limit);
+    return res.status(200).json(launches);
 }
 
 async function httpAddNewLaunch(req, res) {
