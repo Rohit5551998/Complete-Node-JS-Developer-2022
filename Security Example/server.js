3c2157a2cbe04b180f2482164606afdb2d0b459d
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-08 13:50:58 +0530

    Setting up OAuth Cookie Session

--- Security Example/server.js
+++ Security Example/server.js
@@ -3,7 +3,9 @@ const path = require('path');
 const https = require('https');
 const helmet = require('helmet');
 const express = require('express');
+const { verify } = require('crypto');
 const passport = require('passport');
+const cookieSession = require('cookie-session');
 const { Strategy } = require('passport-google-oauth20');
 
 require('dotenv').config();
@@ -13,6 +15,8 @@ const PORT = 3000;
 const config = {
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_SECRET: process.env.CLIENT_SECRET,
+    COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+    COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 }
 
 const AUTH_OPTIONS = {
@@ -31,6 +35,11 @@ passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 const app = express();
 
 app.use(helmet());
+app.use(cookieSession({
+    name: 'session',
+    maxAge: 24 * 60 * 60 * 100,
+    keys: [ config.COOKIE_KEY_1, config.COOKIE_KEY_2 ],
+}))
 app.use(passport.initialize());
 
 function checkLoggedIn(req, res, next) {
