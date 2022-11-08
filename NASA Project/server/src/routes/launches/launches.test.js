abbd863d4c4c03a9cf3b7be732d2545e9501e1e4
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-08 17:12:00 +0530

    Changes for Build Script

--- NASA Project/server/src/routes/launches/launches.test.js
+++ NASA Project/server/src/routes/launches/launches.test.js
@@ -1,10 +1,12 @@
 const request = require('supertest');
 const app = require('../../app');
+const { loadPlanetsData } = require('../../models/planets.models');
 const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
 
 describe('Launches API', () => {
     beforeAll(async () => {
         await mongoConnect();
+        await loadPlanetsData(;)
     });
 
     afterAll(async () => {
