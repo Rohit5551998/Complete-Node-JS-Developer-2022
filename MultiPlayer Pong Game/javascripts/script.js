d8df4887b12ff0fd3d4983fb2488fbf108d20c1c
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-10 17:32:47 +0530

    Implementing Game Logic for Paddle

--- MultiPlayer Pong Game/javascripts/script.js
+++ MultiPlayer Pong Game/javascripts/script.js
@@ -202,6 +202,9 @@ function startGame() {
     if (paddleX[paddleIndex] > (width - paddleWidth)) {
       paddleX[paddleIndex] = width - paddleWidth;
     }
+    socket.emit('paddleMove', {
+      xPosition: paddleX[paddleIndex],
+    });
     // Hide Cursor
     canvas.style.cursor = 'none';
   });
@@ -219,4 +222,10 @@ socket.on('startGame', (refereeId) => {
 
   isReferee = socket.id === refereeId;
   startGame();
+});
+
+socket.on('paddleMove', (paddleData) => {
+  //Toggle 1 into 0 and 0 into 1
+  const opponentPaddleIndex = 1 - paddleIndex;
+  paddleX[opponentPaddleIndex] = paddleData.xPosition;
 });
