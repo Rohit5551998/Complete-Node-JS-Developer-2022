09192f4188392a3f2af5f1dfa18d5ee581bd9763
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-11 17:33:03 +0530

    Typescript Introduction

---
+++ TypeScript/typescript1.js
@@ -0,0 +1,7 @@
+function sum1(a, b) {
+    return a + b;
+}
+var sum2 = function (a, b) {
+    return a + b;
+};
+console.log(sum1(4, 5));
---
+++ TypeScript/typescript1.ts
@@ -0,0 +1,9 @@
+function sum1(a:number, b:number) {
+    return a + b;
+}
+
+const sum2 = (a:number, b:number) => {
+    return a + b;
+}
+
+console.log(sum1(4, 5));
